9cc6fca8cdcee9c37bfdf0b526fd08c22f19eff1
SpeedWagon12 <151126812+SpeedWagon12@users.noreply.github.com>
2023-11-23 10:31:45 +0300

    Add files via upload

---
+++ Zadanie 2/Sasageo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sasageo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(Box1.Text, out double a))
+            { MessageBox.Show("Поле только для чисел"); }
+            if (a > 0)
+            { a = a - 8;
+                Answer.Text += a;
+            }
+            else { Answer.Text += a; }
+
+        }
+    }
+}
